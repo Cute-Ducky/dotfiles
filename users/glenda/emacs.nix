d79dedbfbbdfbf386e2f0f896dceb3a3e778244a
9glenda <luis.braun07@gmail.com>
2022-07-02 17:13:43 +0200

    maybe fix

--- users/glenda/emacs.nix
+++ users/glenda/emacs.nix
@@ -3,6 +3,7 @@
 let
   doom-emacs = pkgs.callPackage (builtins.fetchTarball {
     url = https://github.com/vlaci/nix-doom-emacs/archive/master.tar.gz;
+    sha256 = "sha256:1g0izscjh5nv4n0n1m58jc6z27i9pkbxs17mnb05a83ffdbmmva6";
   }) {
     doomPrivateDir = ./doom.d;  # Directory containing your config.el init.el
                                 # and packages.el files
