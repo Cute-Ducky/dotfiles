3ec71d21633c3dd9b09e7d950b189bc40fc01b7a
9glenda <utody0plz@mozmail.com>
2022-07-16 19:36:48 +0200

    tainscale

--- system/firewall.nix
+++ system/firewall.nix
@@ -11,13 +11,5 @@ networking.firewall = {
     { from = 1714; to = 1764; }
   ];
 
-    # always allow traffic from your Tailscale network
-    trustedInterfaces = [ "tailscale0" ];
-
-    # allow the Tailscale UDP port through the firewall
-    allowedUDPPorts = [ config.services.tailscale.port ];
-
-    # allow you to SSH in over the public internet
-    allowedTCPPorts = [ 22 ];
-  };
+      };
 }
