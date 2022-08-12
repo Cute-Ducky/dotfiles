c889b42f8b74b88f112b5038c8c2a883483985f2
9glenda <utody0plz@mozmail.com>
2022-08-12 15:05:25 +0200

    removed host name in networking.nix

--- system/networking.nix
+++ system/networking.nix
@@ -1,7 +1,6 @@
 { ... }:
  
 {
-networking.hostName = "nixos"; # Define your hostname.
 # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
 networking.networkmanager.enable = true;  
