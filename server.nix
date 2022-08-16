5442347ba855f107256c235caac2ce201e7db26b
9glenda <utody0plz@mozmail.com>
2022-08-16 19:42:40 +0200

    jellyfin

--- server.nix
+++ server.nix
@@ -6,17 +6,19 @@
       ./system/server/hardware-configuration.nix
       ./system/nextcloud.nix
       ./system/ssh.nix
-      ./system/boot.nix
+      ./system/systemd-boot.nix
       ./system/networking.nix
       ./system/users.nix
       ./system/server/doas.nix
       ./system/configuration.nix
       ./system/jellyfin.nix
+      ./system/server/jellyfin.nix
       ./system/firewall.nix
       ./system/tainscale.nix 
       ./system/flakes.nix
       ./system/server/packages.nix
       ./system/minecraft.nix
+      ./system/libvirt.nix
    ];
    networking.hostName = "server-vm";
 }
