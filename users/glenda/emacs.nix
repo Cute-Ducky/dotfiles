40be1fdc964a660eac662d2bd0775ff5f1bcd21b
9glenda <utody0plz@mozmail.com>
2022-08-16 11:27:19 +0200

    emacs

--- users/glenda/emacs.nix
+++ users/glenda/emacs.nix
@@ -11,38 +11,38 @@ home.file = {
   recursive = true;
 };
   ".emacs-profiles.el".text = ''
-(("default" . ((user-emacs-directory . "~/.config/rational-emacs"))))
+(("default" . ((user-emacs-directory . "~/.config/crafted-emacs"))))
   '';
 
-  ".config/rational-emacs/rational-emacs/config.el" = {
+  ".config/crafted-emacs/crafted-emacs/config.el" = {
   text = ''
-(require 'rational-defaults)    ; Sensible default settings for Emacs
-(require 'rational-use-package) ; Configuration for `use-package`
-;(require 'rational-updates)     ; Tools to upgrade Rational Emacs
-(require 'rational-completion)  ; selection framework based on `vertico`
-(require 'rational-ui)          ; Better UI experience (modeline etc.)
-;(require 'rational-windows)     ; Window management configuration
-(require 'rational-editing)     ; Whitspace trimming, auto parens etc.
-(require 'rational-evil)        ; An `evil-mode` configuration
-(require 'rational-org)         ; org-appear, clickable hyperlinks etc.
-;(require 'rational-project)     ; built-in alternative to projectile
-(require 'rational-speedbar)    ; built-in file-tree
-;(require 'rational-screencast)  ; show current command and binding in modeline
+(require 'crafted-defaults)    ; Sensible default settings for Emacs
+(require 'crafted-use-package) ; Configuration for `use-package`
+;(require 'crafted-updates)     ; Tools to upgrade Crafted Emacs
+(require 'crafted-completion)  ; selection framework based on `vertico`
+(require 'crafted-ui)          ; Better UI experience (modeline etc.)
+;(require 'crafted-windows)     ; Window management configuration
+(require 'crafted-editing)     ; Whitspace trimming, auto parens etc.
+(require 'crafted-evil)        ; An `evil-mode` configuration
+(require 'crafted-org)         ; org-appear, clickable hyperlinks etc.
+;(require 'crafted-project)     ; built-in alternative to projectile
+(require 'crafted-speedbar)    ; built-in file-tree
+;(require 'crafted-screencast)  ; show current command and binding in modeline
 
 ;; Further settings and customizations follow here...
-(rational-package-install-package 'nix-mode)
+(crafted-package-install-package 'nix-mode)
 
-(rational-package-install-package 'yaml-mode)
+(crafted-package-install-package 'yaml-mode)
 
 
-(rational-package-install-package 'doom-themes)
+(crafted-package-install-package 'doom-themes)
 (load-theme 'doom-one t)
 
 (setq make-backup-files nil) ; stop creating backup~ files
 (setq auto-save-default nil) ; stop creating #autosave# files
 
 (custom-set-variables
-   '(rational-ui-default-font
+   '(crafted-ui-default-font
      '(:font "JetBrains Mono" :weight light :height 200)))
 (use-package which-key
   :defer 0
@@ -54,13 +54,13 @@ home.file = {
 '';
   recursive = true;
   };
-  ".config/rational-emacs/rational-emacs/ed.el".source = ./ed-mode.el;
-  ".config/rational-emacs" = {
+  ".config/crafted-emacs/crafted-emacs/ed.el".source = ./ed-mode.el;
+  ".config/crafted-emacs" = {
    source = pkgs.fetchFromGitHub {
      owner = "SystemCrafters";
-     repo = "rational-emacs";
-     rev = "3c18e6d";
-     sha256 = "sha256-YERbThmpD+IRF591glMtE2n0E/IzFoJ+2quZyKK9yx4=";
+     repo = "crafted-emacs";
+     rev = "bfaa8ba";
+     sha256 = "sha256-VZtelEPvxKzb4X/W7a0+WhGoBpLSsbgnTAJ9tWrd8Y8=";
   };
   recursive = true;
   };
