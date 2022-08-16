982b21281f50e1a670172c3f867f3ab421711e27
9glenda <utody0plz@mozmail.com>
2022-08-16 11:28:34 +0200

    block all in firewall

--- system/firewall.nix
+++ system/firewall.nix
@@ -3,15 +3,15 @@
 {
 networking.firewall = {
   enable = true;
-  allowedTCPPorts = [ 22 80 8096 ];
+  #allowedTCPPorts = [ 22 80 8096 ];
 ####################################
-  allowedUDPPortRanges = [        #
-    { from = 1714; to = 1764; }   #
-  ];                              #
-  allowedTCPPortRanges = [        #
-    { from = 1714; to = 1764; }   #
-  ];                              #
+#  allowedUDPPortRanges = [        #
+#    { from = 1714; to = 1764; }   #
+#  ];                              #
+#  allowedTCPPortRanges = [        #
+#    { from = 1714; to = 1764; }   #
+#  ];                              #
 ####################################
 
-      };
+   };
 }
