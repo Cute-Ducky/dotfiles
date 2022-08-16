500acca05e65e7e11cddd3c9e9ade93372e29552
9glenda <utody0plz@mozmail.com>
2022-08-16 11:27:34 +0200

    vm-server

---
+++ vm-server.nix
@@ -0,0 +1,22 @@
+{ ... }:
+{
+   imports = [
+      ./system/docker.nix
+      ./system/server/hostname.nix
+      ./system/nextcloud.nix
+      ./system/server/hardware-configuration.nix
+      ./system/ssh.nix
+      ./system/boot.nix
+      ./system/networking.nix
+      ./system/users.nix
+      ./system/server/doas.nix
+      ./system/configuration.nix
+      ./system/firewall.nix
+      ./system/tainscale.nix
+      ./system/key.nix
+      ./system/flakes.nix
+      ./system/server/packages.nix
+   ];
+
+   networking.hostName = "vm-server";
+}
