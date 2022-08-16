a155a53eb1984618aea0e0e15f80a1df831aee79
9glenda <utody0plz@mozmail.com>
2022-08-16 13:31:20 +0200

    tty font

--- system/configuration.nix
+++ system/configuration.nix
@@ -43,7 +43,7 @@ time.timeZone = "Europe/Berlin";
 # Select internationalisation properties.
 i18n.defaultLocale = "en_US.UTF-8";
 console = {
-  font = "Lat2-Terminus16";
+  font = "source-code-pro16";
   keyMap = "us";
 };
 
