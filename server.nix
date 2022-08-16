378ef9d79647b7cabcb3460e3a81dcb4ef6ec609
9glenda <utody0plz@mozmail.com>
2022-08-16 11:27:31 +0200

    server

--- server.nix
+++ server.nix
@@ -1,29 +1,22 @@
 { ... }:
 {
-      imports = [
-./system/docker.nix
-./system/server/hostname.nix
-./system/nextcloud.nix
-./system/server/hardware-configuration.nix
-./system/systemd-boot.nix
-./system/nextcloud.nix
-./system/ssh.nix
-#./system/boot.nix
-./system/networking.nix
-#./system/xorg.nix
-#./system/packages.nix
-#./system/zfs.nix
-./system/users.nix
-./system/server/doas.nix
-./system/configuration.nix
-#./system/flash-os/steam.nix
-./system/jellyfin.nix
-./system/firewall.nix
-./system/tainscale.nix
-./system/key.nix
-./system/flakes.nix
-./system/server/packages.nix
-./system/minecraft.nix
-
-      ];
+   imports = [
+      ./system/docker.nix
+      ./system/nextcloud.nix
+      ./system/server/hardware-configuration.nix
+      ./system/nextcloud.nix
+      ./system/ssh.nix
+      ./system/boot.nix
+      ./system/networking.nix
+      ./system/users.nix
+      ./system/server/doas.nix
+      ./system/configuration.nix
+      ./system/jellyfin.nix
+      ./system/firewall.nix
+      ./system/tainscale.nix 
+      ./system/flakes.nix
+      ./system/server/packages.nix
+      ./system/minecraft.nix
+   ];
+   networking.hostName = "server-vm";
 }
