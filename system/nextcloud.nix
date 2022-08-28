5f9a038df252478f8de139242960e72fc9ff0f0f
9glenda <utody0plz@mozmail.com>
2022-08-28 19:02:32 +0200

    nextcloud

--- system/nextcloud.nix
+++ system/nextcloud.nix
@@ -5,6 +5,6 @@ services.nextcloud = {
   enable = true;                   
   package = pkgs.nextcloud24;
   hostName = "100.72.219.114";
-  config.adminpassFile = "${pkgs.writeText "adminpass" "password1234"};
+  config.adminpassFile = "${pkgs.writeText "adminpass" "nextcloud_password123"};
 };
 }
