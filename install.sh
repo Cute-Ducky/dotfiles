69167644204f247d2f9d27470531b523f012658e
9glenda <utody0plz@mozmail.com>
2022-08-31 20:13:19 +0200

    install script

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 disk="/dev/sda"
+extra="" # for nvme0n1 and partitions being nvme0n1p1 set extra to p
 boot="$disk"
+boot+="$extra"
 boot+="1"
 root="$disk"
+root+="$extra"
 root+="2"
 
 echo "$boot"
